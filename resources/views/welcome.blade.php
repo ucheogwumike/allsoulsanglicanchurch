f0a5770e531f62b63310a20922e0fc612dbb4dd0
Uche Ogwumike <ucheogwumike@gmail.com>
2024-06-18 05:52:18 +0100

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -380,7 +380,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Reverend Dele Adebamiji</h5>
+        <h5 class="modal-title" id="exampleModalLabel">The Right Reverend Dr. Felix Orji</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
